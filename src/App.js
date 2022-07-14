2f7e5c582b9e1a3cfabf0ba1d25a827b40fc0129
Ritika Malik <ritikamalik100102@gmail.com>
2022-07-14 19:04:18 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import { BrowserRouter,  Routes, Route } from 'react-router-dom';
-import ContactForm from './Components/contact/contact';
-import Navigation from './Components/navigation/navigation'
 import Landing from './Pages/landing/landing'
 
 function App() {
   return (
-    <BrowserRouter>
     <Routes>
     <Route path="/" element={<Landing/>}></Route>
     </Routes>
-    </BrowserRouter>
 
   );
 }
