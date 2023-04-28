b2baa04231ef665d82de7df815834a6528152209
Ritika Malik <ritikamalik100102@gmail.com>
2023-04-29 00:07:09 +0530

    animated cursor

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import { Routes, Route } from 'react-router-dom';
 import Landing from './Pages/landing/landing.jsx'
 import ScrollToTop from 'react-scroll-to-top';
+import AnimatedCursor from "react-animated-cursor"
 
 function App() {
   return (
     <div>
+      <AnimatedCursor 
+            innerSize={20}
+            outerSize={20}
+            color='185,194,223'
+            outerAlpha={0.5}
+            innerScale={0.7}
+            outerScale={5}
+            />
          <ScrollToTop />
          <Routes>
          <Route path="/" element={<Landing/>}></Route>
